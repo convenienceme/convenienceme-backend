1f2b3b83f1367e2b988f4b902fed0c314105dcff
Jayaram Sreevalsan <jay@spireminds.com>
2013-06-02 06:03:25 -0400

    baseline

--- src/main/java/me/convenience/api/controller/FormController.java
+++ src/main/java/me/convenience/api/controller/FormController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 /**
@@ -23,8 +22,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
  *
  */
 @Controller
-@RequestMapping("/form")
-@SessionAttributes("credentials")
+@RequestMapping("/form") 
 public class FormController {
 	
 
